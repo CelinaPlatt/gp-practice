49c3c109a5e54144e999bcbb88888fc6a3fe68e9
CelinaPlatt <83833600+CelinaPlatt@users.noreply.github.com>
2022-04-14 15:30:57 +0100

    Initial commit

---
+++ src/app/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+import { fetchNotifications, selectAllNotifications } from '../features/notifications/notificationsSlice'
+
+
+
+export const Navbar = () => {
+  const dispatch = useDispatch()
+  const notifications = useSelector(selectAllNotifications)
+  const numUnreadNotifications = notifications.filter(n => !n.read).length
+
+  const fetchNewNotifications = () => {
+    dispatch(fetchNotifications())
+  }
+
+  let numUnreadNotificationsBadge
+
+  if(numUnreadNotifications > 0) {
+    numUnreadNotificationsBadge = (
+      <span className='badge'>{numUnreadNotifications}</span>
+    )
+  }
+
+  return (
+    <nav>
+      <section>
+        <h1>Redux Essentials Example</h1>
+
+        <div className="navContent">
+          <div className="navLinks">
+            <div className="navLinks">
+              <Link to="/">Posts</Link>
+              <Link to="/users">Users</Link>
+              <Link to="/notifications">Notification{numUnreadNotificationsBadge}</Link>
+            </div>
+            <button className="button" onClick={fetchNewNotifications}>
+              Refresh Notifications
+            </button>
+          </div>
+        </div>
+      </section>
+    </nav>
+  )
+}
